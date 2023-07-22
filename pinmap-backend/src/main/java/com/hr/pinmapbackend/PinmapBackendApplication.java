8fd15ca425dd3f2f7462102d21d245061770487a
Rizvi <s.rizvi@dxc.com>
2023-07-22 18:35:54 +0530

    Uploading to Github

---
+++ pinmap-backend/src/main/java/com/hr/pinmapbackend/PinmapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hr.pinmapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinmapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinmapBackendApplication.class, args);
+	}
+
+}
---
+++ pinmap-backend/src/test/java/com/hr/pinmapbackend/PinmapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.pinmapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinmapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
